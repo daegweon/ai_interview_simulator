70cbb65566af6deef0bc3b4f5d196ef7f4c83814
고대권 <kingkdg@naver.com>
2018-05-02 01:15:46 +0900

    changed the model attributes to camel notation and made it more clear

--- project/models.py
+++ project/models.py
@@ -3,22 +3,22 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-    groupid = models.IntegerField()
-    userid = models.CharField(max_length=100)
+    groupId = models.IntegerField()
+    userId = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
-    fname = models.CharField(max_length=20)
-    lname = models.CharField(max_length=10)
+    firstName = models.CharField(max_length=20)
+    lastName = models.CharField(max_length=10)
     gender  = models.BooleanField()
-    bdate = models.DateField()
+    birthDate = models.DateField()
     email = models.CharField(max_length=100)
-    ldate = models.DateField()
+    loginDate = models.DateField()
 
 class Interview(models.Model):
     emotion = models.CharField(max_length=150)
     speech = models.CharField(max_length=1000)
     tendency = models.CharField(max_length=150)
-    interview_date = models.DateField()
+    interviewDate = models.DateField()
 
 class Question(models.Model):
     question = models.CharField(max_length=100)
-    question_type = models.CharField(max_length=10)
+    questionType = models.CharField(max_length=10)
