a3f77a94b98571b6eb37037a629f8b596391d509
고대권 <kingkdg@naver.com>
2018-05-02 17:39:22 +0900

    URL initialization

--- project/views.py
+++ project/views.py
@@ -4,3 +4,27 @@ from django.shortcuts import render
 
 def main(request):
     return render(request,'project/main.html',{})
+
+def signup(request):
+    return render(request,'project/main.html',{})
+
+def signin(request):
+    return render(request,'project/main.html',{})
+
+def getInterviewPage(request):
+    return render(request,'project/main.html',{})
+
+def trainingInterview(request):
+    return render(request,'project/main.html',{}) 
+
+def testInterview(request):
+    return render(request,'project/main.html',{})   
+
+def getTrainingResultPage(request):
+    return render(request,'project/main.html',{})  
+
+def getTestResultPage(request):
+    return render(request,'project/main.html',{})  
+
+def getRecordPage(request):
+    return render(request,'project/main.html',{})      
