9dca8c1f614cabd136e279c28d5906cfe2812c28
PeachIcetea29 <ijn29@naver.com>
2018-05-02 18:04:36 +0900

    add admin page for question update

--- project/admin.py
+++ project/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User
+from .models import Interview
+from .models import Question
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Interview)
+admin.site.register(Question)
