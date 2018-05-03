aedba8f4b0d1622f55c4c1c66f84341e1c1cba58
김하륜 <khl9411@ajou.ac.kr>
2018-05-03 20:16:11 +0900

    Sign Up & Log In page version.2 (Model modified & birthDate added)

--- project/forms.py
+++ project/forms.py
@@ -8,12 +8,12 @@ class LoginForm(forms.ModelForm):
         fields = ['username', 'password'] # 로그인 시에는 유저이름과 비밀번호만 입력 받는다.
 
 class SignUpForm(UserCreationForm):
-    groupid = forms.IntegerField()
-    fname = forms.CharField(max_length=20)
-    lname = forms.CharField(max_length=10)
+    groupId = forms.IntegerField()
+    firstName = forms.CharField(max_length=20)
+    lastName = forms.CharField(max_length=10)
     gender = forms.BooleanField()
-    #bdate = forms.DateField(help_text='Required. Format: YYYY-MM-DD')
+    birthDate = forms.DateField(help_text='Required. Format: YYYY-MM-DD')
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2', 'groupid', 'fname', 'lname', 'gender']
+        fields = ['username', 'email', 'password1', 'password2', 'groupId', 'firstName', 'lastName', 'gender', 'birthDate']
--- project/models.py
+++ project/models.py
@@ -7,12 +7,12 @@ from django.dispatch import receiver
 
 class Users(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name ='profile')
-    groupid = models.IntegerField(default=5, blank=True)
-    fname = models.CharField(max_length=20, default='a', blank=True)
-    lname = models.CharField(max_length=10, default='d', blank=True)
+    groupId = models.IntegerField(default=5, null=True)
+    firstName = models.CharField(max_length=20, null=True)
+    lastName = models.CharField(max_length=10, null=True)
     gender = models.NullBooleanField(default=True)
-    #bdate = models.DateField(default=None, blank=True)
-    ldate = models.DateField(auto_now=True, blank=True)
+    birthDate = models.DateField(default=None, null=True)
+    loginDate = models.DateTimeField(auto_now=True, blank=True)
 
 @receiver(post_save, sender=User)
 def update_user_profile(sender, instance, created, **kwargs):
