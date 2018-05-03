4e155463c9c7cb7471506729be611758bf05fba4
PeachIcetea29 <ijn29@naver.com>
2018-05-03 19:35:42 +0900

    add mainpage prototype

--- project/admin.py
+++ project/admin.py
@@ -5,6 +5,21 @@ from .models import Question
 
 # Register your models here.
 
+class QuestionAdmin(admin.ModelAdmin):
+    list_per_page = 5
+    list_display = (
+        'id', 'question', 'questionType', 
+    )
+    #list_editable = ('questionType', )
+    list_filter = ('questionType', )
+    search_fields = ('question', )
+    odering = ('id', 'question', )
+    #fields = ('question', 'questionType', )
+    fieldsets = [
+        ('질문 유형', {'fields': ['questionType']}),
+        ('질문 내용', {'fields': ['question']}),
+    ]
+
 admin.site.register(User)
 admin.site.register(Interview)
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
--- project/views.py
+++ project/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def main(request):
-    return render(request,'project/main.html',{})
+    return render(request,'project/index.html',{})
