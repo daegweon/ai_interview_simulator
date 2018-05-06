05348162f10952f254e42680880516eff76aa781
고대권 <kingkdg@naver.com>
2018-05-06 14:36:25 +0900

    Apply Microsoft Face API on the server

--- project/views.py
+++ project/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+import webfacekeras
+import frame_analyze
+from django.http import StreamingHttpResponse
 # Create your views here.
 
 def main(request):
@@ -12,9 +14,11 @@ def signin(request):
     return render(request,'project/main.html',{})
 
 def getInterviewPage(request):
+    frame_analyze.main()
     return render(request,'project/testCameraAudio.html',{})
 
 def trainingInterview(request):
+    webfacekeras.main()
     return render(request,'project/main.html',{}) 
 
 def testInterview(request):
