7e67c7a04bd8137fc0f58d4df0135d5bb87c2e4e
Kim Haryun <khl9411@ajou.ac.kr>
2018-05-13 01:57:15 +0900

    bucket upload test v.2

--- uploadtest.py
+++ uploadtest.py
@@ -1,5 +1,9 @@
+import os
+import sys
 from google.cloud import storage
 
+
+os.system('gcloud auth activate-service-account --key-file key.json')
 
 storage_client = storage.Client()
 bucket = storage_client.get_bucket('capdi_test')
