d87afc368468d339a9a4cc87c4d8e58a1197c70a
Kim Haryun <khl9411@ajou.ac.kr>
2018-05-14 21:01:32 +0900

    speech API apply version.1

--- project/interview/processing/views.py
+++ project/interview/processing/views.py
@@ -5,6 +5,10 @@ from django.http import HttpResponse,HttpResponseRedirect
 import os
 from threading import Thread
 from . import face_analyze
+import sys
+import json
+from google.cloud import storage
+import subprocess
 # Create your views here.
 
 @require_http_methods("POST")
@@ -15,6 +19,29 @@ def videoProcessing(request):
         f_vid.write(video_stream)
 
     os.system('ffmpeg -i testvideo.webm -vf fps=1/3 img%03d.jpg')    
-    os.system('ffmpeg -y -i testvideo.webm -vn testaudio.flac') #옵션 설명 : y : 같은 이름 overwrite , -vn : 음성에 비디오를 포함하지 않겠다. 음성을 포함할 경우 변환시간 오래걸림
+    os.system('ffmpeg -y -i testvideo.webm -sample_rate 16000 -vn testaudio.flac') #옵션 설명 : y : 같은 이름 overwrite , -vn : 음성에 비디오를 포함하지 않겠다. 음성을 포함할 경우 변환시간 오래걸림
     #face_analyze.analyze()
+    speechID = speechProcessing('testaudio.flac','testupload.flac') # 해당 음성 파일에 대한 분석 call의 고유 번호. 이 번호를 기억해야 결과를 가져올 수 있음.
     return HttpResponse('good')
+
+def speechProcessing(src_filename, dst_filename):
+
+    storage_client = storage.Client.from_service_account_json('key.json')   #key.json 이라는 service-account-key 파일이 있어야함.
+    bucket = storage_client.get_bucket('capdi_test')
+    blob = bucket.blob(dst_filename)
+
+    blob.upload_from_filename(src_filename)
+
+    print('File {} uploaded to {}.'.format(
+        src_filename,
+        dst_filename))
+
+    # Speech to text 요청
+    result = json.loads(subprocess.check_output("gcloud ml speech recognize-long-running gs://capdi_test/testupload.flac --language-code=ko-KR --encoding=flac --sample-rates=16000 --async", shell=True))
+
+    #분석이 끝날 때까지 대기하게 되므로 중간에 process 중간에 실행하게 될 경우 시간이 소요될 수 있음. 분석이 끝났는지 확인하기 위해서는 gcloud ml speech operations describe 활용
+    result2 = json.loads(subprocess.check_output("gcloud ml speech operations wait %s"%result['name'],shell=True))
+    for x in range(0,len(result2['results'])):
+        print(result2['results'][x]['alternatives'][0]['transcript'])
+
+    return result['name']
--- uploadtest.py
+++ uploadtest.py
@@ -1,26 +1,49 @@
 import os
+import io
 import sys
 import json
 from google.cloud import storage
 import subprocess
+from google.cloud import speech_v1p1beta1 as speech
 
+client = speech.SpeechClient()
+
+metadata = speech.types.RecognitionMetadata()
+metadata.interaction_type = (speech.enums.RecognitionMetadata.InteractionType.PRESENTATION)
+
+audio = speech.types.RecognitionAudio(uri='gs://capdi_test/testupload.flac')
+config = speech.types.RecognitionConfig(encoding=speech.enums.RecognitionConfig.AudioEncoding.FLAC,
+    sample_rate_hertz=16000,
+    language_code='ko-KR',
+    # Add this in the request to send metadata.
+    metadata=metadata)
+
+operation = client.long_running_recognize(config, audio)
+
+response = operation.result(timeout=90)
+
+for result in response.results:
+    # The first alternative is the most likely one for this portion.
+    print(u'Transcript: {}'.format(result.alternatives[0].transcript))
+    print('Confidence: {}'.format(result.alternatives[0].confidence))
+""" 
 src_filename = 'testaudio.m4a'
 dst_filename = 'testUpload.m4a'
 
-#구글 스토리지 업로드
-#storage_client = storage.Client.from_service_account_json('key.json')   #key.json 이라는 service-account-key 파일이 있어야함.
-#bucket = storage_client.get_bucket('capdi_test')
-#blob = bucket.blob(dst_filename)
+storage_client = storage.Client.from_service_account_json('key.json')   #key.json 이라는 service-account-key 파일이 있어야함.
+bucket = storage_client.get_bucket('capdi_test')
+blob = bucket.blob(dst_filename)
 
-#blob.upload_from_filename(src_filename)
+blob.upload_from_filename(src_filename)
 
 print('File {} uploaded to {}.'.format(
     src_filename,
     dst_filename))
+
 # Speech to text 요청
-#result = json.loads(subprocess.check_output("gcloud ml speech recognize-long-running gs://capdi_test/testUpload2.m4a --language-code=ko-KR --encoding=linear16 --sample-rate=48000 --async", shell=True))
-#result = json.loads(subprocess.check_output("gcloud ml speech recognize-long-running gs://capdi_test/testUpload3.flac --language-code=ko-KR --async", shell=True))
-# 결과 대기 후 출력
-#result2 = subprocess.check_output("gcloud ml speech operations wait %s"%result['name'],shell=True)
-result2 = json.loads(subprocess.check_output("gcloud ml speech operations wait 1912905154630698383",shell=True))
-print(result2['results'][0]['alternatives'][0]['transcript'])
+#result = json.loads(subprocess.check_output("gcloud ml speech recognize-long-running gs://capdi_test/testupload.flac --language-code=ko-KR --encoding=flac --sample-rates=16000 --async", shell=True))
+
+#분석이 끝날 때까지 대기하게 되므로 중간에 process 중간에 실행하게 될 경우 시간이 소요될 수 있음. 분석이 끝났는지 확인하기 위해서는 gcloud ml speech operations describe 활용
+result2 = json.loads(subprocess.check_output("gcloud ml speech operations wait 7024684980943663976",shell=True))
+for x in range(0,len(result2['results'])):
+    print(result2['results'][x]['alternatives'][0]['transcript']) """
