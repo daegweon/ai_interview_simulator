ae34e89e875f351165d9ac82e29dbbddee39285a
고대권 <kingkdg@naver.com>
2018-05-21 19:29:17 +0900

    apply multiprocessing

--- project/interview/processing/speechToText.py
+++ project/interview/processing/speechToText.py
@@ -4,7 +4,8 @@ import subprocess
 
 def speechProcessing(src_filename, dst_filename):
 
-    storage_client = storage.Client.from_service_account_json('key.json')   #key.json 이라는 service-account-key 파일이 있어야함.
+    subprocess.call("gsutil -m mv -r testaudio.flac gs://capdi_test/testupload2.flac",shell=True)
+    '''storage_client = storage.Client.from_service_account_json('key.json')   #key.json 이라는 service-account-key 파일이 있어야함.
     bucket = storage_client.get_bucket('capdi_test')
     blob = bucket.blob(dst_filename)
 
@@ -12,10 +13,10 @@ def speechProcessing(src_filename, dst_filename):
 
     print('File {} uploaded to {}.'.format(
         src_filename,
-        dst_filename))
+        dst_filename))'''
 
     # Speech to text 요청
-    result = json.loads(subprocess.check_output("gcloud ml speech recognize-long-running gs://capdi_test/testupload.flac --language-code=ko-KR --encoding=flac --sample-rate=48000 --async", shell=True))
+    result = json.loads(subprocess.check_output("gcloud ml speech recognize-long-running gs://capdi_test/testaudio.flac --language-code=ko-KR --encoding=flac --sample-rate=48000 --async", shell=True))
 
     #분석이 끝날 때까지 대기하게 되므로 중간에 process 중간에 실행하게 될 경우 시간이 소요될 수 있음. 분석이 끝났는지 확인하기 위해서는 gcloud ml speech operations describe 활용
     result2 = json.loads(subprocess.check_output("gcloud ml speech operations wait %s"%result['name'],shell=True))
--- project/interview/processing/views.py
+++ project/interview/processing/views.py
@@ -9,9 +9,10 @@ from random import randint
 from . import speechToText
 from . import personality
 from . import face_analyze
+from multiprocessing import Process
 import sys
 import os
-
+import time
 # Create your views here.
 
 @require_http_methods("POST")
@@ -29,7 +30,7 @@ def videoProcessing(request):
     if not os.path.isdir('./frames'):
         os.mkdir('./frames')
        
-    os.system('ffmpeg -i '+ video_filename +' -vf fps=1/3 ./frames/img%d.jpg')    
+    os.system('ffmpeg -i '+ video_filename +' -vf fps=1/1 ./frames/img%d.jpg')    
     os.system('ffmpeg -y -i %s -vn %s'%(video_filename, audio_filename)) #옵션 설명 : y : 같은 이름 overwrite , -vn : 음성에 비디오를 포함하지 않겠다. 음성을 포함할 경우 변환시간 오래걸림
 
     #print(request.user) #AnonymousUser 로그인안하면 이렇게 출력된다.
@@ -42,14 +43,24 @@ def videoProcessing(request):
     
     Interview.objects.create(user_id=user_id,question_id=questionId,emotion='',speech='',tendency='',interview_count=interviewObj.interview_count,interview_date = datetime.now(), interview_type = '1')
     interview_id = Interview.objects.values_list('id', flat=True).get(question_id=questionId,interview_count = interviewObj.interview_count)
-    face_analyze.ReqAnalyze(interview_id)
-
-    speechResult = speechToText.speechProcessing(audio_filename, upload_filename)
-    tendency = personality.personality_insights(speechResult)
-    Interview.objects.filter(id=interview_id).update(speech=speechResult, tendency=tendency)
-
+    start_time = time.time()
+    p1 = Process(target=face,args=(interview_id,))
+    p2 = Process(target=audio,args=(interview_id,audio_filename,upload_filename,))
+    p1.start()
+    p2.start()
+    p1.join()
+    p2.join()
+    print("--- %s seconds ---" % (time.time() - start_time))
     #speechResult = speechToText.speechProcessing('testaudio.flac','testupload.flac')
     #personality.personality_insights(speechResult)
-    
     return HttpResponse('good')
 
+def face(interview_id):
+    print("{0} - 프로세스 ID: {1} (부모 프로세스 ID: {2})".format(interview_id, os.getpid(), os.getppid()))
+    face_analyze.ReqAnalyze(interview_id)
+
+def audio(interview_id,audio_filename,upload_filename):
+    print("{0} - 프로세스 ID: {1} (부모 프로세스 ID: {2})".format(interview_id, os.getpid(), os.getppid())) 
+    speechResult = speechToText.speechProcessing(audio_filename, upload_filename)
+    tendency = personality.personality_insights(speechResult)  
+    Interview.objects.filter(id=interview_id).update(speech=speechResult, tendency=tendency)      
