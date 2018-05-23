9cccb37e678937328f8f5ebbbeadacb7a99de602
PeachIcetea29 <ijn29@naver.com>
2018-05-23 18:05:13 +0900

    add words count

--- project/interview/processing/exportWord.py
+++ project/interview/processing/exportWord.py
@@ -18,28 +18,3 @@ def get_tags(text, ntags=50):
     return return_list
  
 #tags = get_tags(text, noun_count) # get_tags 함수 실행
- 
-def main():
-    text_file_name = "out.txt"
-    # 분석할 파일
-    noun_count = 25
-    # 최대 많은 빈도수 부터 20개 명사 추출
-    output_file_name = "count.txt"
-    # count.txt 에 저장
-    open_text_file = open(text_file_name, 'r',-1,"utf-8")
-    # 분석할 파일을 open 
-    text = open_text_file.read() #파일을 읽습니다.
-    tags = get_tags(text, noun_count) # get_tags 함수 실행
-    print(tags)
-    open_text_file.close()   #파일 close
-    open_output_file = open(output_file_name, 'w',-1,"utf-8")
-    # 결과로 쓰일 count.txt 열기
-    for tag in tags:
-        noun = tag['tag']
-        count = tag['count']
-        open_output_file.write('{} {}\n'.format(noun, count))
-    # 결과 저장
-    open_output_file.close() 
- 
-if __name__ == '__main__':
-    main()
--- project/interview/test/views.py
+++ project/interview/test/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 from project.interview.models import Interview, Question,InterviewCount
 import json
 from random import randint
+from project.interview.processing import exportWord
 # Create your views here.
+
 
 def testInterviewHwCheck(request):
     return render(request, 'project/interview/testCam.html', {})
@@ -24,11 +26,18 @@ def testInterviewOnAir(request):
             random_question_text = Question.objects.values_list('question', flat=True).all()[random_idx]
             ques_text.append(random_question_text)
 
-    print(ques_id)
     return render(request,'project/interview/onAir.html',{'ques_id': ques_id, 'ques_text' : ques_text})
 
 def getTestResultPage(request):
+    text = ""
+    noun_count = 3
+    questionList = []
     interview_count = InterviewCount.objects.values_list('interview_count',flat=True).filter(user_id = request.user) 
     emotionResult = Interview.objects.values('emotion').filter(user_id = request.user,interview_count=24)
-    rowcount = emotionResult.count()
-    return render(request,'project/interview/interviewResult.html',{'emotionResult':emotionResult,'rowcount':rowcount})
+    speechResult = Interview.objects.values('speech').filter(user_id = request.user,interview_count=24)
+    for speech in speechResult:
+        text += speech['speech']
+        text += " "
+
+    words = exportWord.get_tags(text, noun_count)
+    return render(request,'project/interview/interviewResult.html',{'emotionResult':emotionResult, 'words':words})
