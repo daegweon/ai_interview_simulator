8ce8e914b39430a88e9ccd5fec808f6789e5cbf1
PeachIcetea29 <ijn29@naver.com>
2018-06-06 18:38:29 +0900

    add comment

--- project/interview/processing/personality.py
+++ project/interview/processing/personality.py
@@ -1,3 +1,11 @@
+##################################
+# 1. 파일명: personality.py
+# 2. 저자 : Human Learning
+# 3. 목적 : 사용자의 답변 내용으로부터 성향 분석 요청
+# 4. 참조 : IBM Personality Insights API
+# 5. 제한(restriction) : subscription key 필요, watson module 필요
+##################################
+
 from watson_developer_cloud import PersonalityInsightsV3
 from os import path
 import json
@@ -5,14 +13,15 @@ import json
 def personality_insights(text):
   key = ""
   with open("personalityKey.txt", 'r') as f:
-    key = f.readline().strip()
+    key = f.readline().strip()  #파일로 부터 Key를 얻어옴
 
+  #API와 Connection 설정
   personality_insights = PersonalityInsightsV3(
     version='2017-10-13',
     username='7c4ae7c8-3137-4469-aa4d-0a653e4c30a5',
     password= key)
 
-  #with open(path.join(path.dirname(__file__), './test.txt')) as profile_txt: (텍스트를 파일로 읽을때 사용. 아래쪽의 text 자리에 profile_text.read()로 대체하면 됨)
+  #성향 분석 요청
   profile = personality_insights.profile(
     text, content_type='text/plain;charset=cp949',
     content_language='ko', accept_language='ko',
